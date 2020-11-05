ca91e4cafe75769b0ed110c5f26ff25c492ed326
Lyu <app@live.jp>
2020-11-06 00:22:06 +0900

    fix

--- services/src/database/models.py
+++ services/src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, FLOAT, DateTime, func
+from sqlalchemy import Column, Integer, FLOAT, String, DateTime, func
 from database.database import BASE
 
 
@@ -6,13 +6,15 @@ class Pricing(BASE):
     __tablename__ = "pricing"
 
     id = Column(Integer, primary_key=True)
+
+    timestamp = Column(DateTime(timezone=True), nullable=False)
+    asset = Column(String, nullable=False)
     open = Column(FLOAT, nullable=False)
     high = Column(FLOAT, nullable=False)
     low = Column(FLOAT, nullable=False)
     close = Column(FLOAT, nullable=False)
     volume = Column(FLOAT, nullable=False)
 
-    timestamp = Column(DateTime(timezone=True), nullable=False)
     created_on = Column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
@@ -23,10 +25,11 @@ class Pricing(BASE):
         onupdate=func.now(),
     )
 
-    def __init__(self, open, high, low, close, volume, timestamp):
+    def __init__(self, timestamp, asset, open, high, low, close, volume):
+        self.timestamp = timestamp
+        self.asset = asset
         self.open = open
         self.high = high
         self.low = low
         self.close = close
         self.volume = volume
-        self.timestamp = timestamp
