b2c7b1084592ac5982abbc1a00a5dca7766c0dd0
Lyu <app@live.jp>
2020-11-18 21:55:15 +0900

    minor fix

--- services/src/data_collector/data_collector.py
+++ services/src/data_collector/data_collector.py
@@ -19,14 +19,18 @@ initialize_main_logger()
 class DataCollector:
     usecase = Usecase()
     binance_cli: ccxt.binance = ccxt.binance(
-        {"enableRateLimit": True, "options": {"defaultType": "future"}}
+        {
+            "enableRateLimit": True,
+            "options": {"defaultType": "future"},
+            "hedgeMode": True,
+        }
     )
 
     def __post_init__(self):
         DB.init()
 
         self._set_target_coins()
-        self._sync_historical_pricing()
+        self._sync_historical_pricing(now=pd.Timestamp.utcnow())
 
     def _set_target_coins(self):
         list_coins_on_binance = sorted(self.binance_cli.fetch_tickers().keys())
@@ -38,11 +42,11 @@ class DataCollector:
             ]
         )
 
-    def _build_inserts_dict_to_sync(self, limit: int):
+    def _build_inserts_dict_to_sync(self, now: pd.Timestamp, limit: int):
         inserts_pricings = []
         synced_timestamps = None
         for asset in self.target_coins:
-            pricing = self._list_historical_pricing(symbol=asset, limit=limit)
+            pricing = self._list_historical_pricing(now=now, symbol=asset, limit=limit)
 
             for pricing_row in pricing.reset_index(drop=False).to_dict(
                 orient="records"
@@ -71,15 +75,19 @@ class DataCollector:
 
         return (inserts_pricings, inserts_syncs)
 
-    def _sync_historical_pricing(self, limit=1500):
-        inserts_pricings, inserts_syncs = self._build_inserts_dict_to_sync(limit=limit)
+    def _sync_historical_pricing(self, now: pd.Timestamp, limit: int = 1500):
+        inserts_pricings, inserts_syncs = self._build_inserts_dict_to_sync(
+            now=now, limit=limit
+        )
 
         self.usecase.insert_pricings(inserts=inserts_pricings)
         self.usecase.insert_syncs(inserts=inserts_syncs)
         logger.info(f"[+] Synced: historical pricings")
 
-    def _sync_live_pricing(self, limit=10):
-        inserts_pricings, inserts_syncs = self._build_inserts_dict_to_sync(limit=limit)
+    def _sync_live_pricing(self, now: pd.Timestamp, limit: int = 10):
+        inserts_pricings, inserts_syncs = self._build_inserts_dict_to_sync(
+            now=now, limit=limit
+        )
 
         self.usecase.update_pricings(updates=inserts_pricings)
         self.usecase.update_syncs(updates=inserts_syncs)
@@ -90,7 +98,9 @@ class DataCollector:
         self.usecase.delete_old_records(table="syncs", limit=1500)
         self.usecase.delete_old_records(table="trades", limit=1500)
 
-    def _list_historical_pricing(self, symbol, limit=1500):
+    def _list_historical_pricing(
+        self, now: pd.Timestamp, symbol: str, limit: int = 1500
+    ):
         assert limit < 2000
 
         if limit >= 1000:
@@ -118,7 +128,8 @@ class DataCollector:
         ).tz_localize("UTC")
 
         # We drop one value always
-        return pricing.sort_index()[:-1]
+        pricing = pricing.sort_index()
+        return pricing[pricing.index < now.floor("T")]
 
     def _get_minutes_to_sync(self, now: pd.Timestamp):
         # Give 1 second waiting term
@@ -133,7 +144,7 @@ class DataCollector:
     def run(self):
         """Definitioin of demon to live sync
         """
-        logger.info("[+] Start: demon of data_collector")
+        logger.info("[O] Start: demon of data_collector")
         while True:
             try:
                 now = pd.Timestamp.utcnow()
@@ -142,7 +153,7 @@ class DataCollector:
                 if minutes_to_sync != 0:
                     minutes_to_sync = min(max(minutes_to_sync, 10), 1500)
 
-                    self._sync_live_pricing(limit=minutes_to_sync)
+                    self._sync_live_pricing(now=now, limit=minutes_to_sync)
                     logger.info(f'[+] Synced: {now.floor("T")}')
             except:
                 logger.info(f"[!] Synced Failed")
